8a1ac94990b5fb406aa12cbb8557cb5ea21f8dd7
shangyuan.ning <shangyuan.ning@manaowan.com>
2019-11-12 15:51:58 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+interface Item {
+  title: number,
+}
+
+const Index: React.FC<Item> = ({ title }: Item) => {
   return (
     <div className="App">
       <header className="App-header">
@@ -16,10 +27,28 @@ const App: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React { title }
         </a>
       </header>
     </div>
+  );
+};
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/1">
+          <Index title={1} />
+        </Route>
+        <Route path="/2">
+          <Index title={2} />
+        </Route>
+        <Route path="/3">
+          <Index title={3}/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
