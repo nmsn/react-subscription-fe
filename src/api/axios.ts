7428eb442d9b6c446efe5abee2d71903d6a71d39
nmsn <136696700@qq.com>
2020-02-11 22:00:38 +0800

    feat(request): 更改权限字段大小写

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -6,11 +6,9 @@ axios.interceptors.request.use((config) => {
   
   // TODO 获取 token
   const token = localStorage.getItem('token');
-  console.log(token);
   
   if (token && token !== 'undefined') {
-    config.headers.Authorization = 'bearer ' +token;
-    // Authorization
+    config.headers.authorization = token;
   }
   // } else {
   //   window.location.href = '/login';
@@ -22,8 +20,8 @@ axios.interceptors.request.use((config) => {
 
 axios.interceptors.response.use(
   (response) => {
-    console.log(response);
-    const token = response.headers.Authorization;
+
+    const token = response.headers.authorization;
     const preToken = localStorage.getItem('token');
     
     if (token !== preToken) {
